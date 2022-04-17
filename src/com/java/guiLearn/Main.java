c5dd24fc41589243bcd185fdabd5b66ee13cbeb2
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-04-17 17:07:25 +0530

    :fire: completed the project

---
+++ src/com/java/guiLearn/Main.java
@@ -0,0 +1,9 @@
+package com.java.guiLearn;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arrival = {0,1,2,3,4};
+        int[] burst = {7,5,1,2,8};
+        SJFNP xyz = new SJFNP(arrival,burst);
+    }
+}
